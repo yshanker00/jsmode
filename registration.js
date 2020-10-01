27495239f4be8bfc06a1d3ec139ee95a5582b3a6
yshanker00 <c-syerragu@pa.gov>
2020-10-01 17:22:58 -0400

    added html and js file

---
+++ registration.js
@@ -0,0 +1,4 @@
+// Tasks for today :Implement a registration page and 
+// store details of each registered user in local storage.
+// Username, password,  mobile number and email are the fields
+// Make sure,  unique emails should be stored
